56abbfd1cedf983cb12a44763210984c03f49fd0
sachahjkl <sacha@sacha.house>
2026-09-12 19:17:54 +0200

    Expose the application as a Nix template

--- template/flake.nix
+++ template/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Nix application template";
+  description = "Application template";
 
   inputs = {
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2605";
@@ -10,12 +10,27 @@
   };
 
   outputs = {
+    self,
     nixpkgs,
     git-hooks,
-    ...
   }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
+    root = pkgs.runCommand "application-root" {} ''
+      mkdir -p "$out/etc/caddy" "$out/srv"
+      cp ${./Caddyfile} "$out/etc/caddy/Caddyfile"
+      cp -R ${./site}/. "$out/srv/"
+    '';
+    dockerImage = pkgs.dockerTools.buildLayeredImage {
+      name = "application";
+      tag = "latest";
+      created = "1970-01-01T00:00:01Z";
+      contents = [pkgs.caddy root];
+      config = {
+        Cmd = ["${pkgs.caddy}/bin/caddy" "run" "--config" "/etc/caddy/Caddyfile"];
+        ExposedPorts."8080/tcp" = {};
+      };
+    };
     preCommitCheck = git-hooks.lib.${system}.run {
       package = pkgs.prek;
       src = ./.;
@@ -30,12 +45,18 @@
       };
     };
   in {
-    templates.default = {
-      path = ./template;
-      description = "Application with Nix build and deployment workflows";
+    packages.${system} = {
+      default = dockerImage;
+      inherit dockerImage;
     };
-    checks.${system}.pre-commit = preCommitCheck;
+
+    checks.${system} = {
+      inherit dockerImage;
+      pre-commit = preCommitCheck;
+    };
+
     formatter.${system} = pkgs.alejandra;
+
     devShells.${system}.default = pkgs.mkShell {
       packages = preCommitCheck.enabledPackages;
       inherit (preCommitCheck) shellHook;
